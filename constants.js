249080883d7525e18186be6b12869ca36b204374
Haziq Fariduddin <98182768+HaziqFariduddin@users.noreply.github.com>
2023-02-26 15:40:50 +0800

    Version 1
    A minimal client for stable difussion hosted inference API on Replicate

---
+++ constants.js
@@ -0,0 +1,3 @@
+export const MODEL_NAME = 'stability-ai/stable-diffusion'
+export const MODEL_VERSION = 'db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf'
+export const TIMEOUT = 10
---
+++ index.js
@@ -0,0 +1,65 @@
+import { fetch, CookieJar } from 'node-fetch-cookies'
+import * as constants from './constants.js'
+
+function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+
+export default async function stablediffusion(prompt, inputs = {}) {
+
+    const sessionCookieJar = new CookieJar()
+
+    // Grab a CSRF token...
+
+    await fetch(sessionCookieJar, `https://replicate.com/${constants.MODEL_NAME}`)
+
+    // Call the model API...
+
+    const response1 = await fetch(sessionCookieJar, `https://replicate.com/api/models/${constants.MODEL_NAME}/versions/${constants.MODEL_VERSION}/predictions`, {
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+            'x-csrftoken': sessionCookieJar.cookies.get('csrftoken'),
+        },
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+        body: JSON.stringify({
+            inputs: {
+                guidance_scale: '7',
+                width: 512,
+                height: 512,
+                num_inference_steps: 50,
+                num_outputs: 1,
+                seed: null,
+                prompt,
+                ...inputs,
+            },
+        }),
+    })
+
+    // Wait for the image to be generated...
+
+    const uuid = (await response1.json()).uuid
+
+    for (let timeoutCounter = 0; timeoutCounter < constants.TIMEOUT; timeoutCounter++) {
+
+        let response2 = await fetch(sessionCookieJar, `https://replicate.com/api/models/${constants.MODEL_NAME}/versions/${constants.MODEL_VERSION}/predictions/${uuid}`, {
+            headers: {
+                'accept': '*/*',
+            },
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'include',
+            body: null,
+        })
+
+        let output = (await response2.json())?.prediction?.output
+        if (output && output.length) { return output }
+
+        await sleep(1000)
+    }
+
+    return []
+}
